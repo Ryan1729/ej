be9e0d2db58efeba82bb7bdf019ad53e24be8547
Ryan1729 <Ryan1729@gmail.com>
2023-10-05 01:06:00 -0600

    get immediate terminal input working, and multiple other changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{
     fs::{self},
-    io::{self, Read, Write},
+    io::{self, ErrorKind, Read, Write},
     os::unix::net::{UnixListener, UnixStream},
     path::PathBuf,
+    sync::mpsc::{channel, Receiver, TryRecvError},
+    thread,
 };
 
 type Res<A> = Result<A, Box<dyn std::error::Error>>;
 
-const SOCKET_PATH: &str = "/tmp/ej-socket";
+const SOCKET_PATH: &str = "ej-socket";
 
 fn do_client() -> Res<()> {
     let socket = PathBuf::from(SOCKET_PATH);
@@ -30,17 +32,107 @@ fn do_server() -> Res<()> {
     }
 
     let listener = UnixListener::bind(&socket)?;
+    listener.set_nonblocking(true)?;
+
+    fn setup_terminal() -> io::Result<()> {
+        use std::os::fd::AsRawFd;
+
+        let tty;
+        let fd = if unsafe { libc::isatty(libc::STDIN_FILENO) } == 1 {
+            libc::STDIN_FILENO
+        } else {
+            tty = fs::File::open("/dev/tty")?;
+
+            tty.as_raw_fd()
+        };
+
+        let mut ptr = core::mem::MaybeUninit::uninit();
+
+        if unsafe { libc::tcgetattr(fd, ptr.as_mut_ptr()) } == 0 {
+            let mut termios = unsafe { ptr.assume_init() };
+            let c_oflag = termios.c_oflag;
+
+            unsafe { libc::cfmakeraw(&mut termios); }
+            termios.c_oflag = c_oflag;
+            // Enable signal handling again after cfmakeraw disabled it
+            termios.c_lflag |= libc::ISIG;
+
+            if unsafe { libc::tcsetattr(fd, libc::TCSADRAIN, &termios) } == 0 {
+                return Ok(());
+            }
+        }
+
+        Err(io::Error::last_os_error())
+    }
+
+    setup_terminal()?;
 
     println!("Server started, waiting for clients");
 
     let mut buffer = String::with_capacity(1024);
+    let mut jump_points = Vec::with_capacity(16);
+    let mut index = 0;
 
-    // Iterate over clients, blocks if no client available
-    for client in listener.incoming() {
-        client?.read_to_string(&mut buffer)?;
-        println!("Client said: {buffer}");
+    struct JumpPoint {
+        path: PathBuf,
+    }
 
-        buffer.clear();
+    fn parse_jump_points(jump_points: &mut Vec<JumpPoint>, to_parse: &str) {
+        // TODO actual parsing
+        //for line in to_parse.lines() {
+            //
+        //}
+    }
+
+    let stdin_channel: Receiver<u8> = {
+        let (tx, rx) = channel::<u8>();
+        thread::spawn(move || {
+            let mut stdin = io::stdin();
+            let mut buffer = [0; 1];
+            loop {
+                stdin.read_exact(&mut buffer).unwrap();
+                tx.send(buffer[0]).unwrap();
+            }
+        });
+        rx
+    };
+
+    loop {
+        // Iterate over clients, blocks if no client available
+        match listener.accept() {
+            Ok((mut stream, _)) => {
+                stream.read_to_string(&mut buffer)?;
+                println!("Client said: {buffer}");
+
+                parse_jump_points(&mut jump_points, &buffer);
+
+                buffer.clear();
+            },
+            Err(e) if e.kind() == ErrorKind::WouldBlock => {}
+            Err(e) => println!("accept function failed: {e:?}"),
+        }
+
+        const SPACE: u8 = 32;
+
+        match stdin_channel.try_recv() {
+            Ok(SPACE) => match jump_points.get(index) {
+                Some(_jp) => println!("TODO implement jumping"),
+                None => println!("No jump point found"),
+            },
+            Ok(key) => println!("Received: {}", key),
+            Err(TryRecvError::Empty) => {},
+            Err(e) => Err(e)?,
+        }
+
+        // TODO accept input and display UI
+        // UI:
+        //     List of jump points
+        //         Space to trigger current selection and move to next
+        //         Arrows to move along list
+        //     Some way to view the raw input?
+
+        // TODO calculate amount to sleep based on how long things took
+        thread::sleep(std::time::Duration::from_millis(16));
     }
 
     Ok(())
